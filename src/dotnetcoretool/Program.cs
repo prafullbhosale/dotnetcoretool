803e8ddcb64278d02f5bcb02f774ef9316d5d954
Prafull Bhosale <prbhosal@microsoft.com>
2017-01-13 11:00:04 -0800

    Create project structure

---
+++ src/dotnetcoretool/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+
+namespace dotnetcoretool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication()
+            {
+                Name = "dotnetcoretool",
+                Description = "dotnetcoretool"
+            };
+
+            app.HelpOption("-h|--help");
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 1;
+            });
+
+            app.Execute(args);
+        }
+    }
+}
